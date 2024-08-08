dda2be03dca8b84791b44273dcef59b45600de3d
nikita3011 <nikii.ghule@gmail.com>
2024-08-08 21:19:04 +0100

    classes section complete

--- gym-typescript/src/App.tsx
+++ gym-typescript/src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits" 
+import OurClasses from "@/scenes/ourClasses"
 import { useEffect, useState } from "react";
 import { SelectedPage } from "./shared/types";
 
@@ -42,6 +43,9 @@ const handleScroll = () =>{
     setSelectedPage={setSelectedPage}
     children
    />
+    <OurClasses
+    setSelectedPage = {setSelectedPage}
+    />
       </div>
   );
 }
